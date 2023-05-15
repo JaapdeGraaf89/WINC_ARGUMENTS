1a8ece50685b02a4035fbac0a5f02c9e8f54c295
JaapdeGraaf89 <114916265+JaapdeGraaf89@users.noreply.github.com>
2023-05-15 20:32:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Do not modify these lines
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
+
+# Add your code after this line
+def greet(name,greeting="Hello"):
+    greeting = (f"{greeting} {name}")
+    return greeting
+    
+print(greet("Bob"))
+
